f00d1ec96f9aed608dfae96e1c46a69dcdca064b
PIYUSH KUMAR ANAND <piyushanandemail@gamil.com.com>
2025-10-08 20:06:29 +0530

    do it

---
+++ trading-platform/src/main/java/com/hackerrank/tradingplatform/TradingPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.hackerrank.tradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradingPlatformApplication.class, args);
+    }
+}
